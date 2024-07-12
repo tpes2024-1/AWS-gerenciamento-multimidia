570874125efead44418606e3f0659dc70da257ab
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-07-12 17:36:27 -0300

    Adicionar esquemas de usuário ao arquivo schemas.py

---
+++ Backend/app/schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+class User(BaseModel):
+    username: str
+    email: Optional[EmailStr] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+class UserInDB(User):
+    hashed_password: str
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Optional[str] = None
+    password: str
