05f4999ee3397318803447c8d061136ffe5f7bb9
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-07-12 17:36:21 -0300

    Adicionar modelo de usuário ao arquivo models.py

---
+++ Backend/app/models.py
@@ -0,0 +1,11 @@
+from typing import Dict
+
+db: Dict[str, dict] = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "john@gmail.com",
+        "hashed_password": "$2b$12$/4we3oYJ5Zjr77UcYPSJNOsjxPUJIxGMufZfgBa3fIYHcztQNQIJq",
+        "disabled": False
+    }
+}
