4e3d581e7be88a316b122f41f72ed070ccfe560d
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-07-13 10:35:49 -0300

    Adicionar rota de registro de usuário em users.py

--- Backend/app/routes/users.py
+++ Backend/app/routes/users.py
@@ -1,13 +1,43 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from ..database import SessionLocal
 from ..schemas import User, UserCreate
-from ..models import db
+from ..models import User as UserModel
 from ..security import get_password_hash, get_current_active_user
 
 router = APIRouter()
 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @router.post("/register", response_model=User)
-async def register_user(user: UserCreate):
-    if user.username in db:
+async def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(UserModel).filter(UserModel.username == user.username).first()
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Nome de usuário já registrado",
+        )
+    hashed_password = get_password_hash(user.password)
+    db_user = UserModel(
+        username=user.username,
+        email=user.email,
+        full_name=user.full_name,
+        hashed_password=hashed_password,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    
+    return db_user
+
+    """ if user.username in db:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Nome de usuário já registrado",
@@ -20,11 +50,13 @@ async def register_user(user: UserCreate):
         "hashed_password": hashed_password,
         "disabled": False,
     }
-    return db[user.username]
+    return db[user.username] """
+
 
 @router.get("/users/me", response_model=User)
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
+
 
 @router.get("/users/me/items")
 async def read_users_me_items(current_user: User = Depends(get_current_active_user)):
