4892853078bd1ceef5304a063874716b23ca9f47
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-07-13 10:32:42 -0300

    Adicionar criação das tabelas no banco de dados e configuração do servidor de desenvolvimento

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from .routes import auth, users
+from .database import SessionLocal, engine, Base
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 app.include_router(auth.router)
 app.include_router(users.router)
+
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
