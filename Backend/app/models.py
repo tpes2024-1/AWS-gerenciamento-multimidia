7a1daf8265f6bc958811f42ce219030fcf6f0f9a
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-07-13 10:32:49 -0300

    Adicionar modelo de usuário ao arquivo models.py

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -1,5 +1,17 @@
 from typing import Dict
+from .database import Base
+from sqlalchemy import Boolean, Column, Integer, String
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    full_name = Column(String)
+    disabled = Column(Boolean, default=False)
 
+
+""" 
 db: Dict[str, dict] = {
     "johndoe": {
         "username": "johndoe",
@@ -8,4 +20,4 @@ db: Dict[str, dict] = {
         "hashed_password": "$2b$12$/4we3oYJ5Zjr77UcYPSJNOsjxPUJIxGMufZfgBa3fIYHcztQNQIJq",
         "disabled": False
     }
-}
+} """
