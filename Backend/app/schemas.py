18fac99b2d46220cb8516f2ce7479e2fa0f6f402
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-07-13 19:15:48 -0300

    Adicionar campo de refresh token ao modelo de Token em schemas.py

--- Backend/app/schemas.py
+++ Backend/app/schemas.py
@@ -6,6 +6,10 @@ from datetime import datetime
 class Token(BaseModel):
     access_token: str
     token_type: str
+    refresh_token: str
+    
+class RefreshTokenRequest(BaseModel):
+    refresh_token: str
 
 class TokenData(BaseModel):
     username: Optional[str] = None
