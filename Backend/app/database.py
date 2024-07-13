7060abb30b5ffdafa2cc45be44adf1f0c4bf5207
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-07-13 10:31:51 -0300

    Adicionar criação das tabelas no banco de dados

---
+++ Backend/app/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./Drive.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Criação das tabelas no banco de dados
+Base.metadata.create_all(bind=engine)
