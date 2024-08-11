4c08de89b3deb12e86af46c57c7f0b85dec96b56
Joao Filipe Batista <joao.filipe@solyd.com.br>
2024-08-11 16:37:26 -0300

    File and image models

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from .database import Base
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, Float, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import JSONB, ARRAY
 
 
 class User(Base):
@@ -15,3 +17,34 @@ class User(Base):
     description = Column(String)
     hashed_password = Column(String)
     disabled = Column(Boolean, default=False)
+
+
+class File(Base):
+    __abstract__ = True
+
+    # Propriedades do arquivo
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    file_name = Column(String, nullable=False)
+    file_size = Column(Integer, nullable=False)  # Em bytes
+    uploaded_at = Column(DateTime, default=datetime.utcnow)
+    description = Column(Text, nullable=True)
+    tags = Column(ARRAY(String), nullable=True)  # Lista de tags
+
+    # Relacionamento com o usuário
+    user = relationship("User", back_populates="files")
+
+
+class Image(File):
+    __tablename__ = 'images'
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    # Propriedades da imagem
+    mime_type = Column(String, nullable=False)
+
+    # Propriedades específicas da imagem
+    width = Column(Integer, nullable=True)  # Largura em pixels
+    height = Column(Integer, nullable=True)  # Altura em pixels
+    color_depth = Column(Integer, nullable=True)  # Profundidade de cor em bits
+    resolution = Column(String, nullable=True)  # DPI (pontos por polegada)
+    exif_data = Column(JSONB, nullable=True)  # Armazenar dados EXIF como JSON
---
+++ Backend/app/routes/images.py
@@ -0,0 +1,26 @@
+import os
+
+import boto3
+from fastapi import APIRouter, Depends, Form, HTTPException, status, UploadFile, File
+from sqlalchemy.orm import Session
+
+from ..database import SessionLocal
+
+router = APIRouter()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv('S3_ACCESS_KEY'),
+    aws_secret_access_key=os.getenv('S3_SECRET_KEY')
+)
+
+bucket_name = os.getenv('AWS_BUCKET_NAME')
