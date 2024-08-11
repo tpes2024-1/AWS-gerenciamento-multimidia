502e0bfbd91bd378f69594dec87529569e317a87
Joao Filipe Batista <joao.filipe@solyd.com.br>
2024-08-11 12:16:55 -0300

    Add DATABASE_URL

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('USER_RDS')}:{os.getenv('PASSWORD_RDS')}@projetotpes.cv4cgo8m627y.sa-east-1.rds.amazonaws.com:5432/postgres"
+SQLALCHEMY_DATABASE_URL = f"{os.getenv('DATABASE_URL')}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
