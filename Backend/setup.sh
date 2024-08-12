39f73a5017d9da234e171ba50f0dfe4549446fc8
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-08-12 10:43:42 -0300

    Fixes

--- Backend/setup.sh
+++ Backend/setup.sh
@@ -27,26 +27,9 @@ pip install -r requirements.txt
 echo "SECRET_KEY=1927153dddcf87154da80948228971e802c70349047a48abe090719f0c04a84e
 ALGORITHM=HS256
 ACCESS_TOKEN_EXPIRE_MINUTES=30
-DATABASE_URL=postgresql://postgres:serverstoreAdmin:qP4cTm2\\$#XbPX3#5a!@localhost/postgres
+DATABASE_URL=postgresql://postgres:serverstoreAdmin:qP4cTm2\$#XbPX3#5a!@localhost/postgres
 S3_ACCESS_KEY=AKIATCKAR6CDF3KREB7G
 S3_SECRET_KEY=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
 AWS_BUCKET_NAME=projeto-tpes" > .env
 
-# Configurar o Uvicorn para iniciar a aplicação
-echo "[Unit]
-Description=Uvicorn instance to serve my FastAPI app
-After=network.target
 
-[Service]
-User=ubuntu
-Group=ubuntu
-WorkingDirectory=/home/ubuntu/seu-repositorio
-EnvironmentFile=/home/ubuntu/seu-repositorio/.env
-ExecStart=/home/ubuntu/myenv/bin/uvicorn main:app --host 0.0.0.0 --port 8000
-
-[Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/myapp.service
-
-# Iniciar o serviço da aplicação
-sudo systemctl start myapp
-sudo systemctl enable myapp
